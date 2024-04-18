f6783e1b7647e43bceb0d0cb88191dbffa83459a
Huang-Nathan <112867798+Huang-Nathan@users.noreply.github.com>
2024-04-18 18:08:15 +0800

    Add files via upload

---
+++ js/about_us.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    // centeredSlides: true,
+    spaceBetween: 20,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   type: "fraction",
+    // },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+})
